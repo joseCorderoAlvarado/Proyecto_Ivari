8cb5087a5f51f9cd4b1b27677b6232512ea525f1
Jose Cordero <josehipolito.c.a@gmail.com>
2019-09-18 18:57:34 -0600

    Create tipodireccion.php

---
+++ iravi/database/seeds/tipodireccion.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class tipodireccion extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('tipodireccion')-> insert(array('tipo'=>'Personal'));
+        DB::table('tipodireccion')-> insert(array('tipo'=>'Envio'));
+        DB::table('tipodireccion')-> insert(array('tipo'=>'Facturacion'));
+    }
+}

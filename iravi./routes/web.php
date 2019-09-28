93670914bca2371d0e14a9d0540d88cc566c54b6
Jose Cordero <josehipolito.c.a@gmail.com>
2019-09-27 22:00:11 -0600

    modificacion de rutas

--- iravi./routes/web.php
+++ iravi./routes/web.php
@@ -35,11 +35,9 @@ Route::get('compras_Carrito', function () {
 Route::get('descripcion_Producto', function () {
     return view('descripcion_Producto',['numero'=>15]);
 });
-
-Route::get('footer', function () {
-    return view('footer',['numero'=>15]);
+Route::get('historial_Ventas', function () {
+    return view('historial_Ventas',['numero'=>15]);
 });
-
 Route::get('index_Admin', function () {
     return view('index_Admin',['numero'=>15]);
 });
@@ -51,6 +49,9 @@ Route::get('login', function () {
 Route::get('modificar_Producto', function () {
     return view('modificar_Producto',['numero'=>15]);
 });
+Route::get('modificar_UsuarioAdm', function () {
+    return view('modificar_UsuarioAdm',['numero'=>15]);
+});
 
 Route::get('perfil_Usuario', function () {
     return view('perfil_Usuario',['numero'=>15]);
@@ -58,4 +59,7 @@ Route::get('perfil_Usuario', function () {
 
 Route::get('registro', function () {
     return view('registro',['numero'=>15]);
+});
+Route::get('seguimiento_Pedidos', function () {
+    return view('seguimiento_Pedidos',['numero'=>15]);
 });

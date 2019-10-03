210ec8fe3a51b4559d9d30de07ca3abc062fe096
Jose Cordero <josehipolito.c.a@gmail.com>
2019-10-02 21:53:48 -0600

    Update web.php

--- iravi./routes/web.php
+++ iravi./routes/web.php
@@ -15,9 +15,24 @@ Route::get('/', function () {
     return view('index');
 });
 
-
 Route::get('alta_Producto', function () {
     return view('alta_Producto',['numero'=>15]);
+});
+
+Route::get('historia', function () {
+    return view('historia',['numero'=>15]);
+});
+
+Route::get('compras_Realizadas', function () {
+    return view('Compras',['numero'=>15]);
+});
+
+Route::get('confirmacion_Compra', function () {
+    return view('confirmacion_Compra',['numero'=>15]);
+});
+
+Route::get('vista_Producto', function () {
+    return view('vista_Producto',['numero'=>15]);
 });
 
 Route::get('alta_Usuarios', function () {
@@ -65,22 +80,4 @@ Route::get('seguimiento_Pedidos', function () {
 });
 Route::get('Cliente', function () {
     return view('index_Cliente',['numero'=>15]);
-});
-Route::get('Historia', function () {
-    return view('historia',['numero'=>15]);
-});
-Route::get('perfil_Usuario', function () {
-    return view('perfil_Usuario',['numero'=>15]);
-});
-Route::get('compras_Realizadas', function () {
-    return view('compras_Realizadas',['numero'=>15]);
-});
-Route::get('vista_Producto', function () {
-    return view('vista_Producto',['numero'=>15]);
-});
-Route::get('confirmacion_Compra', function () {
-    return view('confirmacion_Compra',['numero'=>15]);
-});
-Route::get('registro', function () {
-    return view('registro',['numero'=>15]);
 });

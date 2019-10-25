9a52a0d0a247959cc768050e6fb9edd49faf8104
JCesar <jcesarfb7@gmail.com>
2019-10-25 17:37:08 -0600

    Semilleros restantes

--- iravi/database/seeds/DatabaseSeeder.php
+++ iravi/database/seeds/DatabaseSeeder.php
@@ -17,7 +17,10 @@ class DatabaseSeeder extends Seeder
         $this-> call (roles::class);
         $this-> call (tipodireccion::class);
         $this-> call (Categorias::class);
-        $this-> call (proveedores::class);
-        
+          $this-> call (proveedores::class);
+         $this-> call (persona::class);
+         $this-> call (direccion::class);
+        $this-> call (usuario::class);
+
     }
 }

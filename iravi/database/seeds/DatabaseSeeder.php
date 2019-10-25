693c0d3ae261de247a5db04a2ff0d21f3c6b636e
Javier-Ramirez <javi.guti.rama@gmail.com>
2019-10-25 14:30:15 -0500

    Creación de semillero Proveedores

--- iravi/database/seeds/DatabaseSeeder.php
+++ iravi/database/seeds/DatabaseSeeder.php
@@ -17,6 +17,6 @@ class DatabaseSeeder extends Seeder
         $this-> call (roles::class);
         $this-> call (tipodireccion::class);
         $this-> call (Categorias::class);
-
+        $this-> call (proveedores::class);
     }
 }

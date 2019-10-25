ba1a8fbb748250d9f4dc3e54b6e4448609719611
Javier-Ramirez <javi.guti.rama@gmail.com>
2019-10-25 14:51:13 -0500

    Semillero

--- iravi/database/seeds/proveedores.php
+++ iravi/database/seeds/proveedores.php
@@ -13,28 +13,25 @@ class proveedores extends Seeder
     public function run()
     {
         //
-        DB::table('proveedores')-> insert(array('idproveedores'=>'1'));
-        DB::table('proveedores')-> insert(array('nombreproveedor'=>'PulserasFelipe'));
-        DB::table('proveedores')-> insert(array('sucursal'=>'Ixtlan del Rio'));
-        DB::table('proveedores')-> insert(array('encargado'=>'Felipe Carrillo'));
-        DB::table('proveedores')-> insert(array('fechaingreso'=>'2019-10-01 00:00:00'));
-        DB::table('proveedores')-> insert(array('fechaegreso'=>'2020-09-30 00:00:00'));
-        DB::table('proveedores')-> insert(array('sedeprincipal'=>'Ixtlan del Rio'));
+        DB::table('proveedores')-> insert(string('nombreproveedor'=>'PulserasFelipe'));
+        DB::table('proveedores')-> insert(string('sucursal'=>'Ixtlan del Rio'));
+        DB::table('proveedores')-> insert(string('encargado'=>'Felipe Carrillo'));
+        DB::table('proveedores')-> insert(datetime('fechaingreso'=>'2019-10-01 00:00:00'));
+        DB::table('proveedores')-> insert(datetime('fechaegreso'=>'2020-09-30 00:00:00'));
+        DB::table('proveedores')-> insert(string('sedeprincipal'=>'Ixtlan del Rio'));
 
-        DB::table('proveedores')-> insert(array('idproveedores'=>'2'));
-        DB::table('proveedores')-> insert(array('nombreproveedor'=>'Kiosco'));
-        DB::table('proveedores')-> insert(array('sucursal'=>'Ixtlan del Rio'));
-        DB::table('proveedores')-> insert(array('encargado'=>'Presidencia Municipal Ixtlan del Rio'));
-        DB::table('proveedores')-> insert(array('fechaingreso'=>'2019-10-01 00:00:00'));
-        DB::table('proveedores')-> insert(array('fechaegreso'=>'2020-09-30 00:00:00'));
-        DB::table('proveedores')-> insert(array('sedeprincipal'=>'Ixtlan del Rio'));
+        DB::table('proveedores')-> insert(string('nombreproveedor'=>'Kiosco'));
+        DB::table('proveedores')-> insert(string('sucursal'=>'Ixtlan del Rio'));
+        DB::table('proveedores')-> insert(string('encargado'=>'Presidencia Municipal Ixtlan del Rio'));
+        DB::table('proveedores')-> insert(datetime('fechaingreso'=>'2019-10-01 00:00:00'));
+        DB::table('proveedores')-> insert(datetime('fechaegreso'=>'2020-09-30 00:00:00'));
+        DB::table('proveedores')-> insert(string('sedeprincipal'=>'Ixtlan del Rio'));
 
-        DB::table('proveedores')-> insert(array('idproveedores'=>'3'));
-        DB::table('proveedores')-> insert(array('nombreproveedor'=>'EquipalesMexpan'));
-        DB::table('proveedores')-> insert(array('sucursal'=>'Mexpan'));
-        DB::table('proveedores')-> insert(array('encargado'=>'Señor Mexpeño'));
-        DB::table('proveedores')-> insert(array('fechaingreso'=>'2019-10-01 00:00:00'));
-        DB::table('proveedores')-> insert(array('fechaegreso'=>'2020-09-30 00:00:00'));
-        DB::table('proveedores')-> insert(array('sedeprincipal'=>'Mexpan'));
+        DB::table('proveedores')-> insert(string('nombreproveedor'=>'EquipalesMexpan'));
+        DB::table('proveedores')-> insert(string('sucursal'=>'Mexpan'));
+        DB::table('proveedores')-> insert(string('encargado'=>'Señor Mexpeño'));
+        DB::table('proveedores')-> insert(datetime('fechaingreso'=>'2019-10-01 00:00:00'));
+        DB::table('proveedores')-> insert(datetime('fechaegreso'=>'2020-09-30 00:00:00'));
+        DB::table('proveedores')-> insert(string('sedeprincipal'=>'Mexpan'));
     }
 }

be253c393bfa34cc85e2a9dc0f0f18965a8374cc
Javier-Ramirez <javi.guti.rama@gmail.com>
2019-10-25 14:10:20 -0500

    Semillero Proveedores v1

---
+++ iravi/database/seeds/proveedores.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class proveedores extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('proveedores')-> insert(array('nombreproveedor'=>'Pulseras Felipe'));
+        DB::table('proveedores')-> insert(array('sucursal'=>'Ixtlan del Rio'));
+        DB::table('proveedores')-> insert(array('encargado'=>'Felipe Carrillo'));
+        DB::table('proveedores')-> insert(date('fechaingreso'=>'2019-10-01 00:00:00'));
+        DB::table('proveedores')-> insert(date('fechaegreso'=>'2020-09-30 00:00:00'));
+        DB::table('proveedores')-> insert(date('sedeprincipal'=>'Ixtlan del Rio'));
+    }
+}

a46b650f2a3612f8399ec81f74eee139f7d9abe8
Javier-Ramirez <javi.guti.rama@gmail.com>
2019-10-25 14:45:24 -0500

    Semilla v45

--- iravi/database/seeds/proveedores.php
+++ iravi/database/seeds/proveedores.php
@@ -13,6 +13,7 @@ class proveedores extends Seeder
     public function run()
     {
         //
+        DB::table('proveedores')-> insert(array('idproveedores'=>'1'));
         DB::table('proveedores')-> insert(array('nombreproveedor'=>'PulserasFelipe'));
         DB::table('proveedores')-> insert(array('sucursal'=>'Ixtlan del Rio'));
         DB::table('proveedores')-> insert(array('encargado'=>'Felipe Carrillo'));
@@ -20,6 +21,7 @@ class proveedores extends Seeder
         DB::table('proveedores')-> insert(array('fechaegreso'=>'2020-09-30 00:00:00'));
         DB::table('proveedores')-> insert(array('sedeprincipal'=>'Ixtlan del Rio'));
 
+        DB::table('proveedores')-> insert(array('idproveedores'=>'2'));
         DB::table('proveedores')-> insert(array('nombreproveedor'=>'Kiosco'));
         DB::table('proveedores')-> insert(array('sucursal'=>'Ixtlan del Rio'));
         DB::table('proveedores')-> insert(array('encargado'=>'Presidencia Municipal Ixtlan del Rio'));
@@ -27,6 +29,7 @@ class proveedores extends Seeder
         DB::table('proveedores')-> insert(array('fechaegreso'=>'2020-09-30 00:00:00'));
         DB::table('proveedores')-> insert(array('sedeprincipal'=>'Ixtlan del Rio'));
 
+        DB::table('proveedores')-> insert(array('idproveedores'=>'3'));
         DB::table('proveedores')-> insert(array('nombreproveedor'=>'EquipalesMexpan'));
         DB::table('proveedores')-> insert(array('sucursal'=>'Mexpan'));
         DB::table('proveedores')-> insert(array('encargado'=>'Señor Mexpeño'));

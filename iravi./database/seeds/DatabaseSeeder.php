a965fe22107a2606803decafe7bd876a25628217
José Cordero Alvarado <48689264+joseCorderoAlvarado@users.noreply.github.com>
2019-11-03 16:16:49 -0600

    Commit a sedder y rutas

--- iravi./database/seeds/DatabaseSeeder.php
+++ iravi./database/seeds/DatabaseSeeder.php
@@ -12,12 +12,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+		$this-> call (proveedores::class);
         $this-> call (estatus::class);
         $this-> call (genero::class);
         $this-> call (roles::class);
         $this-> call (tipodireccion::class);
         $this-> call (Categorias::class);
-          $this-> call (proveedores::class);
          $this-> call (persona::class);
          $this-> call (direccion::class);
         $this-> call (usuario::class);

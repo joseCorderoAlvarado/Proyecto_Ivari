70215cc9ccc90dff6b728fb700e53993ec8c1b6d
José Cordero Alvarado <48689264+joseCorderoAlvarado@users.noreply.github.com>
2019-11-06 01:39:11 -0600

    Update head.blade.php

--- iravi/resources/views/layouts/head.blade.php
+++ iravi/resources/views/layouts/head.blade.php
@@ -4,8 +4,8 @@
 		<title>Artesanias Iravi</title>
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	  <link rel="stylesheet" type="text/css" href="css/estilos.css">
-	  <script type="text/javascript" src="js/Validaciones.js"></script>
-		<script src="js/form.js"></script>
+	  <script type="text/javascript" src="js/validaciones.js"></script>
+		<script  type="text/javascript"src="js/form.js"></script>
 		<link rel="stylesheet" type="text/css" href="css/estiloValidaciones.css">
 		 <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
 		  <script type="text/javascript" src="js/main.js"></script>

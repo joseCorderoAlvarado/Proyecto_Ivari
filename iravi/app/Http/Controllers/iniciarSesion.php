61d1afa0571fec3c82ef90323be42a885b00fd85
José Cordero Alvarado <48689264+joseCorderoAlvarado@users.noreply.github.com>
2019-11-08 06:47:56 -0600

    ligeras correcciones

--- iravi/app/Http/Controllers/iniciarSesion.php
+++ iravi/app/Http/Controllers/iniciarSesion.php
@@ -12,11 +12,11 @@ use Illuminate\Http\Request;
 		if (session()->has('S_Rol') ) {
 		return redirect ('/');
 	}else{
-		
+
 		return view ('login');
 	}
 
-}	
+}
 
 		public function verificar (Request $datos)
 		{
@@ -25,71 +25,19 @@ use Illuminate\Http\Request;
 			$persona_v = new persona;
 			$correo_Electronico = $persona_v->txtUsuario = $datos->input ('txtUsuario');
 			$contraseña = $usuario_v->txtContrasena = $datos->input ('txtContrasena');
-
+       echo "$correo_Electronico";
+			 echo "$contraseña";
+			 exit();
 			//echo $correo_Electronico."jajajaja";
 			//echo $contraseña."jejeje";
-			
-			
+
+
 			//$correo_Electronico="javier_agustinrm@hotmail.com";
-			//$contraseña="1234";	
+			//$contraseña="1234";
 
 			//Creamos una varible usuario que se iguala a la tabla usuario
 			//con un inner join que hace referencia a las tablas relacionadas y los id y el fk relacionados
 			//se hace una consulta donde se consultan los campos correo electrónico de la tabla persona y la contraseña de la tabla usuario
 			//se hace la condición del where donde se consultan solo aquellos que posean los datos del correo y contraseña ingresados por el usuario.
 
-			$resultadoUsuario = usuario::
-		    join('persona', 'persona.idpersona', '=', 'usuario.fkpersona')
-		    ->select('persona.correoelectronico', 'usuario.contrasena')
-		    ->where('persona.correoelectronico', '=', $correo_Electronico)
-		    ->where('usuario.contrasena', '=', $contraseña) 
-		    ->get();
-
-		    //Este es el inner join realizado de una manera cosnulta directa a SQL
-			//	select persona.correoelectronico, usuario.contraseña from usuario 
-			//	inner join persona on usuario.fkpersona = persona.idpersona  
-			//	where persona.correoelectronico = $correo_Electronico 
-			//	and usuario.contraseña = $contraseña; 
-
-			if ($fila=($resultadoUsuario)) 
-			{
-				//buscar esos datos en la base de datos
-				$resultadoRol = persona::
-			    join('usuario', 'usuario.idusuario', '=', 'persona.idpersona')
-			    ->select('usuario.fkrol')
-			    ->where('persona.correoelectronico', '=', $correo_Electronico)
-				->where('usuario.contrasena', '=', $contraseña)
-			    ->get();
-				
-				//echo $resultadoRol;
-				
-				//Se va a utilizar la sesion global
-			   if(strpos($resultadoRol, '3') == true){
-				    session(['S_Rol' => '3']);
-
-			       return redirect('/');
-			   }
-			   
-			   elseif  (strpos($resultadoRol, '1') == true){
-				   
-				   session(['S_Rol' => '1']);
-
-			       return redirect('/'); 
-				   
-			   }
-			}
-		
-				 return redirect('login');
-			
-		
-		}
-		
-		
-		public function cerrarSesion (Request $datos)
-		{
-			//Matamos todos los datos de la sesion
-			Session()->flush();
-			  return redirect('/'); 
-		}
-	}
-?>
+	

7d9e94301904379f3482dd70486c0a67d6cf45dd
Elizabeth Aguilar <eliaylin011298@gmail.com>
2019-11-13 15:52:18 -0800

    Solucion registro usuario

--- iravi/app/Http/Controllers/registroUsuarioAdmin.php
+++ iravi/app/Http/Controllers/registroUsuarioAdmin.php
@@ -17,6 +17,43 @@ if (session()->has('S_Rol') ) {
 }
 return redirect ('/');
 }
+public function guardar(Request $datos){
+//dd($datos->all());
+$persona = new persona;
+$persona->nombrepersona=$datos->input('nombre');
+$persona->apellidopaterno=$datos->input('app');
+$persona->apellidomaterno=$datos->input('apm');
+$persona->fechanacimiento=$datos->input('nacimiento');
+$persona->telefono=$datos->input('telefono');
+$persona->correoelectronico=$datos->input('correo');
+$persona->fkgenero=$datos->input('genero');
+$persona->save();
+$ultimoidpersona=$persona->idpersona;
+$usuario = new usuario;
+$usuario->idusuario = $datos->input('idusuario');
+$usuario->contrasena = $datos->input('contrasena');
+$usuario->fkpersona =  $ultimoidpersona;
+$usuario->fkrol = 1;
+$usuario->save();
+$direccion = new direccion;
+$direccion->calle = $datos->input('calle');
+$direccion->numint = $datos->input('ninterior');
+$direccion->numext = $datos->input('nexterior');
+$direccion->colonia = $datos->input('colonia');
+$direccion->municipio = $datos->input('municipio');
+$direccion->ciudad = $datos->input('ciudad');
+$direccion->pais = $datos->input('pais');
+$direccion->codigopostal = $datos->input('cp');
+$direccion->fktipo = $datos->input('tipodireccion');
+$direccion->save();
+$ultimoiddireccion=$direccion->id;
+$detallepersonadireccion= new detallepersonadireccion;
+$detallepersonadireccion->fkpersona=$ultimoidpersona;
+$detallepersonadireccion->fk_direccion=$ultimoiddireccion;
+$detallepersonadireccion->save();
+return redirect('registroUsuarioAdmin');
+}
+
 
 }
 ?>

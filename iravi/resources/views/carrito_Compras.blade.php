849a3f5a885f1fc6c53d618a5183dbe32f118fe3
José Cordero Alvarado <48689264+joseCorderoAlvarado@users.noreply.github.com>
2019-11-20 01:30:15 -0600

    modificaciones diseño carrito de compras

--- iravi/resources/views/carrito_Compras.blade.php
+++ iravi/resources/views/carrito_Compras.blade.php
@@ -1,37 +1,56 @@
 @extends('layouts.head')
 @include('layouts.menu_NavegacionCliente')
+
 <?php
 $totalCarrito = 0;
  ?>
+<div class="container-fluid" style="border: 1px #ccc solid; border-radius: 3px; padding: 10px; margin: auto;">
+   <div class="row">
+      <div class="col-2">
+        <h5 >Imagén</h5>
+      </div>
+      <div class="col-2">
+        <h5 style="text-align:center;">Nombre Producto</h5>
+      </div>
+      <div class="col-2">
+        <h5 style="text-align:center;">Precio Unitario</h5>
+      </div>
+      <div class="col-2">
+        <h5 style="text-align:center;">Cantidad</h5>
+      </div>
+      <div class="col-2">
+        <h5 style="text-align:center;">Subtotal</h5>
+      </div>
+   </div>
+</div>
 @foreach($carrito as $carrito2)
-
-<div class="container" style="border: 1px #ccc solid; border-radius: 3px; padding: 10px; margin: auto;">
-		<div class="row">
-      	<div class="col-md-2">
+<div class="container-fluid" style="border: 1px #ccc solid; border-radius: 3px; padding: 10px; margin: auto;">
+    <div class="row">
+      	<div class="col-2 my-auto">
           		<img src="storage{{$carrito2->ruta}}" width="80em" height="80em" class="d-inline-block align-top">
           </div>
-		<div class="col-md-6">
-
+		<div class="col-2 my-auto">
       <p> Producto: <span style="color: gray" class="d-inline-block align-top">{{$carrito2->nombreproducto}}</span>   </p>
-
+    </div>
+    	<div class="col-2 my-auto">
       <p>  Precio: <span style="color: gray" class="d-inline-block align-top">${{$carrito2->precio}}</span></p>
-  <form action="modificarCarrito" method="post" enctype="multipart/form-data">
+      </div>
+      <div class="col-2 my-auto">
+      <form action="modificarCarrito" method="post" enctype="multipart/form-data">
     		{{ csrf_field() }}
       <p> Cantidad:	  <input style="color: gray" class="d-inline-block align-top" type="number" name="cantidad" value="{{$carrito2->cantidad}}" min="1" max="1000" step="1"/></p>
       <input type="hidden" name="idProducto" value="{{$carrito2->idproducto}}">
-      <p>  <button  type="submit" class="btn btn-danger" style="background:#003366">Modificar</button></p>
+      <p> <button  type="submit" class="btn btn-danger" style="background:#003366">Modificar</button></p>
     </form>
-		</div>
-    	<div class="col-md-2">
-
-          <p> Subtotal: <span style="color: gray" class="d-inline-block align-top">{{$carrito2->precio*$carrito2->cantidad}}</span></p>
+     </div>
+    	<div class="col-md-2 my-auto">
+          <p> Subtotal: <span style="color: gray" class="d-inline-block align-top">${{$carrito2->precio*$carrito2->cantidad}}</span></p>
       </div>
-    <div class="col-md-2">
-    <p>   Eliminar:</p>
+    <div class="col-md-2 my-auto">
     <form action="eliminarCarrito" method="post" enctype="multipart/form-data">
           {{ csrf_field() }}
             <input type="hidden" name="idProducto" value="{{$carrito2->idproducto}}">
-      <input type="image" img src="\images\eliminar.png" width="80em" height="80em" class="d-inline-block align-top">
+      <input type="image" src="images/eliminar.png" width="80em" height="80em" class="class="formulario_btn">
         </form>
 	   </div>
 		</div>
@@ -40,7 +59,7 @@ $totalCarrito = 0;
   $totalCarrito = $totalCarrito + ($carrito2->precio*$carrito2->cantidad);
 	 ?>
 @endforeach
-<div class="container">
+<div class="container-fluid">
 	<div class="row">
 		<div class="col-md-4">
 			Bonificacion:
@@ -61,9 +80,9 @@ $totalCarrito = 0;
 	<div class="row">
 		<div class="col-md-4">
 		</div>
-		<div class="col-md-6">
+		<div class="col-md-4" style="text-align:center;">
 		<a href="{{ url('confirmacionCompra')}}">
-		<button type="button" class="btn btn-primary" style="background:#003366">Proceder al pago </button>
+		<button type="button" class="formulario_btn" style="background:#003366;">Proceder al pago </button>
 		</a>
 		</div>
 		<div class="col-md-2">

b4abb0c6856537c89e9d1a0b5301c16cf36b7825
Javier-Ramirez <javi.guti.rama@gmail.com>
2019-11-24 11:12:47 -0600

    Creacion de vista editar_Direccion.blade.php y Recodificación de perfil_Usuario.blade.php

---
+++ iravi/resources/views/editar_Direccion.blade.php
@@ -0,0 +1,181 @@
+@extends('layouts.head')
+@include('layouts.menu_NavegacionCliente')
+<style type="text/css">
+	input
+	{
+		border: none;
+		background-color: transparent;
+		color: #9FA5AB;
+	}
+	#text {
+		color: #003366; 
+	}
+	#button{
+		background-color: #003366; 
+		border-color: #003366; 
+
+		border-style:solid;
+	}
+	
+	.nav-link.active{
+		background-color:#92d050 !important;
+		border-color: #92d050 !important; 
+		border-style:solid;
+		font-weight: bold;
+	}
+	
+	.card-body{
+		border-color: #003366; 
+		border-style:solid;
+	}
+	
+</style>
+<br><br><br>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-3">
+			<div  class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+				<a  style="color: 92d050; font-weight:bold;" class="nav-link" href="{{ url('comprasRealizadas')}}">
+					Compras realizadas
+				</a>
+				<a  class="nav-link active" href="{{ url('perfilUsuario')}}">
+					Mi Perfil
+				</a>
+				<a  style="color: 92d050; font-weight:bold;" class="nav-link" href="{{ url('historia')}}">
+					Acerca de Iravi
+				</a>
+			</div>
+		</div>
+
+		
+			<div  style="background: transparent; width: 800px; ">
+  				<article class="card-body" >
+  					<h4 id="text">Datos de domicilios</h4>
+					<hr style="margin-bottom: 40px;">
+
+						<form action="modificarDireccion" method="post" enctype="multipart/form-data">
+						{{ csrf_field() }}
+
+						<div class="form-group" style="background-color: #E0E0E0;">
+							<div class="row">
+								<div class="col-4">
+									<label id="text">Calle:</label>
+								</div>
+								<div class="col-6">
+									<input id="text" type="text"  name="txtCalle" contenteditable="false" style=" border: solid; background-color: transparent;" value="{{$direcciones[0]->calle}}">
+								</div>
+							</div>
+							
+							<div class="row">
+								<div class="col-4">
+									<label id="text" >N&uacute;mero interior:</label>
+								</div>
+						
+								<div class="col-6">
+									<input id="text" type="text" name="txtNumeroInt" contenteditable="false" style=" border: solid; background-color: transparent; " value="{{$direcciones[0]->numint}}">
+
+								</div>
+							</div>
+
+							<div class="row">
+								<div class="col-4">
+									<label id="text" >N&uacute;mero exterior:</label>
+								</div>
+						
+								<div class="col-6">
+									<input id="text" type="text" name="txtNumeroExt" contenteditable="false" style=" border: solid; background-color: transparent; " value="{{$direcciones[0]->numext}}">
+
+								</div>
+							</div>
+
+							<div class="row">
+								<div class="col-4">
+									<label id="text">Colonia:</label>
+								</div>
+								<div class="col-6">
+									<input id="text" type="text" name="txtColonia" contenteditable="false" style=" border: solid; background-color: transparent; " value="{{$direcciones[0]->colonia}}">
+								</div>
+							</div>
+							
+							<div class="row">
+								<div class="col-4">
+									<label id="text" >C&oacute;digo P&oacute;stal:</label>
+								</div>
+								<div class="col-6">
+									<input id="text" type="text"  name="txtCodigoP" contenteditable="false"  style=" border: solid; background-color: transparent;"  value="{{$direcciones[0]->codigopostal}}">
+
+								</div>
+							</div>
+
+							<div class="row">
+								<div class="col-4">
+									<label id="text">Municipio:</label>
+								</div>
+								<div class="col-6">
+									<input id="text" type="text"  name="txtMunicipio" contenteditable="false" style=" border: solid; background-color: transparent;"  value="{{$direcciones[0]->municipio}}">
+
+								</div>
+							</div>
+
+							<div class="row">
+								<div class="col-4">
+									<label id="text">Ciudad:</label>
+								</div>
+								<div class="col-6">
+									<input id="text" type="text"  name="txtCiudad" contenteditable="false" style="  border: solid; background-color: transparent;" value="{{$direcciones[0]->ciudad}}">
+
+								</div>
+								
+							</div>
+
+							<div class="row">
+								<div class="col-4">
+									<label id="text">Pais:</label>
+								</div>
+								<div class="col-6">
+									<input id="text" type="text"  name="txtPais" contenteditable="false" style="  border: solid; background-color: transparent;" value="{{$direcciones[0]->pais}}">
+
+								</div>
+								
+							</div>
+
+
+							<div class="row">
+								<div class="col-4">
+									<label id="text">Tipo de dirección:</label>
+								</div>
+
+								<div class="col-6">
+									<select  name="txtTipoDireccion" class="custom-select" style="width: 370px;">
+						 	 			@foreach ($tipoDireccion as $tipoDireccion)
+												<option  value={{ $tipoDireccion->idtipo }}>
+													{{ $tipoDireccion->tipo }} 
+												</option>
+						  				@endforeach
+				       				</select>
+				       			</div>
+				       		</div>
+
+								<div class="col-12" align="center">
+									<br>
+									<input type="hidden" name="txtidPersona1" " value="{{$idPersona}}">
+									<input type="hidden" name="txtfkdireccion" value="{{$direcciones[0]->iddireccion}}">
+									<button role="button" class="btn btn-large btn-primary" data-toggle="modal">Modificar direcci&oacute;n</button> 
+								</div>
+							</div>
+						</div>
+						</form>
+						
+					</hr>
+				</article>
+			</div>
+
+	</div>
+</div>
+
+<div class="col-3">
+			
+</div>
+
+	<br><br><br><br>
+@extends('layouts.footer')
--- iravi/resources/views/perfil_Usuario.blade.php
+++ iravi/resources/views/perfil_Usuario.blade.php
@@ -28,6 +28,8 @@
 		border-color: #003366; 
 		border-style:solid;
 	}
+
+
 	
 </style>
 <br><br><br>
@@ -92,7 +94,7 @@
 								</div>
 								
 								<div class="col-7">
-									<input id="text" type="text" name="txtApellidoP" contenteditable="false" style="width: 400px;  border: none; background-color: transparent; color: #9FA5AB;" value="{{$usuarioDatos[0]->apellidopaterno}}">
+									<input id="text" type="text" readonly="true" name="txtApellidoP" contenteditable="false" style="width: 400px;  border: none; background-color: transparent; color: #9FA5AB;" value="{{$usuarioDatos[0]->apellidopaterno}}">
 								</div>
 								<div class="col-1">
 									<a href="#editarApellidoP" role="button" class="btn btn-large btn-primary" data-toggle="modal">&rarr;</a>
@@ -105,7 +107,7 @@
 								</div>
 				
 								<div class="col-7">
-									<input id="text" type="text" name="txtApellidoM"  contenteditable="false" style="width: 400px; border: none; background-color: transparent; color: #9FA5AB;" value="{{$usuarioDatos[0]->apellidomaterno}}">
+									<input id="text" type="text" readonly="true" name="txtApellidoM"  contenteditable="false" style="width: 400px; border: none; background-color: transparent; color: #9FA5AB;" value="{{$usuarioDatos[0]->apellidomaterno}}">
 								</div>
 								<div class="col-1">
 									<a href="#editarApellidoM" role="button" class="btn btn-large btn-primary" data-toggle="modal">&rarr;</a>
@@ -118,9 +120,15 @@
 							<br>
 							<h5 id="text">Datos de domicilios</h5>
 						</div>
-			
+
+						<?php
+						$i=-1;
+						?>
 						@foreach ($direcciones as $direccion)
-						<form action="modificarDireccion" method="post" enctype="multipart/form-data">
+						<?php
+						$i=$i+1;
+						?>
+						<form action="editarDireccion" method="post" enctype="multipart/form-data">
 						{{ csrf_field() }}
 
 						<div class="form-group" style="background-color: #E0E0E0;">
@@ -129,13 +137,13 @@
 									<label id="text">Calle:</label>
 								</div>
 								<div class="col-4">
-									<input id="text" type="text" name="txtCalle" contenteditable="false" style=" border: none; background-color: transparent; color: #9FA5AB;" value="{{$direccion->calle}}">
+									<input id="text" type="text" readonly="true" name="txtCalle" contenteditable="false" style=" border: none; background-color: transparent; color: #9FA5AB;" value="{{$direccion->calle}}">
 								</div>
 								<div class="col-2">
 									<label id="text" >N&uacute;mero:</label>
 								</div>
 								<div class="col-2">
-									<input id="text" type="text" name="txtNumero" contenteditable="false" style=" border: none; background-color: transparent; color: #9FA5AB;" value="{{$direccion->numint}}">
+									<input id="text" type="text" readonly="true" name="txtNumero" contenteditable="false" style=" border: none; background-color: transparent; color: #9FA5AB;" value="{{$direccion->numint}}">
 
 								</div>
 							</div>
@@ -144,14 +152,14 @@
 									<label id="text">Colonia:</label>
 								</div>
 								<div class="col-4">
-									<input id="text" type="text" name="txtColonia" contenteditable="false" style=" border: none; background-color: transparent; color: #9FA5AB;" value="{{$direccion->colonia}}">
+									<input id="text" type="text" readonly="true" name="txtColonia" contenteditable="false" style=" border: none; background-color: transparent; color: #9FA5AB;" value="{{$direccion->colonia}}">
 
 								</div>
 								<div class="col-2">
 									<label id="text" >C&oacute;digo P&oacute;stal:</label>
 								</div>
 								<div class="col-2">
-									<input id="text" type="text" name="txtCodigoP" contenteditable="false"  style=" border: none; background-color: transparent; color: #9FA5AB;" value="{{$direccion->codigopostal}}">
+									<input id="text" type="text" readonly="true" name="txtCodigoP" contenteditable="false"  style=" border: none; background-color: transparent; color: #9FA5AB;" value="{{$direccion->codigopostal}}">
 
 								</div>
 							</div>
@@ -163,14 +171,14 @@
 									<label id="text">Municipio:</label>
 								</div>
 								<div class="col-4">
-									<input id="text" type="text" name="txtMunicipio" contenteditable="false" style=" border: none; background-color: transparent; color: #9FA5AB;" value="{{$direccion->municipio}}">
+									<input id="text" type="text" readonly="true" name="txtMunicipio" contenteditable="false" style=" border: none; background-color: transparent; color: #9FA5AB;" value="{{$direccion->municipio}}">
 
 								</div>
 								<div class="col-2">
 									<label id="text">Ciudad:</label>
 								</div>
 								<div class="col-2">
-									<input id="text" type="text" name="txtEstado" contenteditable="false" style="width:200px;  border: none; background-color: transparent; color: #9FA5AB;" value="{{$direccion->ciudad}}">
+									<input id="text" type="text" readonly="true" name="txtEstado" contenteditable="false" style="width:200px;  border: none; background-color: transparent; color: #9FA5AB;" value="{{$direccion->ciudad}}">
 
 								</div>
 								
@@ -180,20 +188,22 @@
 									<label id="text">Tipo de dirección:</label>
 								</div>
 								<div class="col-4">
-									<input id="text" type="text" name="txtTipo" contenteditable="false" style=" border: none; background-color: transparent; color: #9FA5AB;" value="{{$direccion->tipo}}">
-
-									<input id="text" type="hidden" name="txtDet" contenteditable="false" style=" border: none; background-color: transparent; color: #9FA5AB;" >
+									<input id="text" type="text" readonly="true" name="txtTipo" contenteditable="false" style=" border: none; background-color: transparent; color: #9FA5AB;" value="{{$direccion->tipo}}">
 
 								</div>
 								<div class="col-12" align="right">
 									<br>
-									<a href="#editarDireccion" role="button" class="btn btn-large btn-primary" data-toggle="modal">Modificar direcci&oacute;n</a>
+									<input type="hidden" name="txtidPersona1" " value="{{$usuarioDatos[0]->idpersona}}">
+									<input type="hidden" name="txtfkdireccion" value="{{$direcciones[$i]->fk_direccion}}">
+									<button role="button" class="btn btn-large btn-primary" data-toggle="modal">Modificar direcci&oacute;n</button> 
 								</div>
 							</div>
 						</div>
 						</form>
 						@endforeach
-
+						<?php
+						$i=-1;
+						?>
 						<div class="form-title" style="color: blue">
 							<a href="#agregarDireccion" role="button" class="btn btn-large btn-primary" data-toggle="modal">Agregar otra dirección</a>
 						</div>
@@ -210,7 +220,7 @@
 									<label id="text">Correo electr&oacute;nico:</label>
 								</div>
 								<div class="col-9">
-									<input id="text" type="text" name="txtCorreoE" contenteditable="false" style="width: 500px; border: none; background-color: transparent; color: #9FA5AB;" value="{{$usuarioDatos[0]->correoelectronico}}">
+									<input id="text" type="text" readonly="true" name="txtCorreoE" contenteditable="false" style="width: 500px; border: none; background-color: transparent; color: #9FA5AB;" value="{{$usuarioDatos[0]->correoelectronico}}">
 
 								</div>
 								<div class="col-1">
@@ -222,7 +232,7 @@
 									<label id="text">Contrase&ntilde;a:</label>
 								</div>
 								<div class="col-9">
-									<input id="text" type="password" contenteditable="false" style=" border: none; background-color: transparent; color: #9FA5AB;" value="{{$usuarioDatos[0]->contrasena}}">
+									<input id="text" type="password" readonly="true" contenteditable="false" style=" border: none; background-color: transparent; color: #9FA5AB;" value="{{$usuarioDatos[0]->contrasena}}">
 								</div>
 								<div class="col-1">
 									<a href="#editarContraseña" role="button" class="btn btn-large btn-primary" data-toggle="modal">&rarr;</a>
@@ -494,126 +504,6 @@
     </div>
 </div>
 
-
-<div id="editarDireccion" class="modal fade">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-            	<h4 class="modal-title">Modificar Direcci&oacute;n</h4>
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>           
-            </div>
-           	
-           	<form  action="modificarDireccion" method="post" enctype="multipart/form-data">
-			{{ csrf_field() }}
-
-           		<div class="modal-body">
-	            	<div class="row">
-						<div class="col-2">
-							<label id="text">Calle:</label>
-						</div>
-								
-						<div class="col-5">
-							<input id="text"type="text" name="txtCalleEditar" style="width: 350px; border: solid; border-color: blue; background-color: transparent; color: black;" value="">
-						</div>
-								
-					</div>
-
-					<div class="row">
-						<div class="col-2">
-							<label id="text">Número Interior:</label>
-						</div>
-								
-						<div class="col-5">
-							<input id="text"type="text" name="txtNumIntEditar" style="width: 350px; border: solid; border-color: blue; background-color: transparent; color: black;" value="">
-						</div>
-								
-					</div>
-
-					<div class="row">
-						<div class="col-2">
-							<label id="text">Número Exterior:</label>
-						</div>
-								
-						<div class="col-5">
-							<input id="text"type="text" name="txtNumExtEditar" style="width: 350px; border: solid; border-color: blue; background-color: transparent; color: black;" value="">
-						</div>
-								
-					</div>
-
-					<div class="row">
-						<div class="col-2">
-							<label id="text">Colonia:</label>
-						</div>
-								
-						<div class="col-5">
-							<input id="text"type="text" name="txtColoniaEditar" style="width: 350px; border: solid; border-color: blue; background-color: transparent; color: black;" value="">
-						</div>
-								
-					</div>
-
-					<div class="row">
-						<div class="col-2">
-							<label id="text">Municipio:</label>
-						</div>
-								
-						<div class="col-5">
-							<input id="text"type="text" name="txtMunicipioEditar" style="width: 350px; border: solid; border-color: blue; background-color: transparent; color: black;" value="">
-						</div>
-								
-					</div>
-
-					<div class="row">
-						<div class="col-2">
-							<label id="text">Ciudad:</label>
-						</div>
-								
-						<div class="col-5">
-							<input id="text"type="text" name="txtCiudadEditar" style="width: 350px; border: solid; border-color: blue; background-color: transparent; color: black;" value="">
-						</div>
-								
-					</div>
-
-					<div class="row">
-						<div class="col-2">
-							<label id="text">Pais:</label>
-						</div>
-								
-						<div class="col-5">
-							<input id="text"type="text" name="txtPaisEditar" style="width: 350px; border: solid; border-color: blue; background-color: transparent; color: black;">
-						</div>
-								
-					</div>
-
-					<div class="row">
-						<div class="col-2">
-							<label id="text">Código Postal:</label>
-						</div>
-								
-						<div class="col-5">
-							<input id="text"type="text" name="txtCodigoPEditar" style="width: 350px; border: solid; border-color: blue; background-color: transparent; color: black;" value="">
-						</div>
-								
-					</div>
-
-					<div class="row">
-						<div class="col-2">
-							<label id="text">Tipo de dirección:</label>
-						</div>
-
-						
-		       		</div>
-
-	            	<p class="text-warning"><small>No dejar este campo vacio.</small></p>
-	       		</div>
-	       		<input type="hidden" name="txtidPersona" value="{{$usuarioDatos[0]->idpersona}}">
-	            
-	        	<div class="modal-footer">
-					<button id="button" class="btn btn-primary" style="background-color: #003669; color: white;">Guardar Cambios</button>
-				</div>
-	        </form>   
-        </div>
-    </div>
-</div>
 
 <div class="col-3">
 			

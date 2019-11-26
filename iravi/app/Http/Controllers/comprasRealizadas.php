f2d7358274686e3bc906842c28b94c4cdd743c04
ramon diaz <ramondiaz123@yahoo.com>
2019-11-25 20:56:01 -0700

    Correccion del paginado de los pedidos

--- iravi/app/Http/Controllers/comprasRealizadas.php
+++ iravi/app/Http/Controllers/comprasRealizadas.php
@@ -8,39 +8,40 @@ use DB;
 
 class comprasRealizadas extends Controller
 {
-	
+
 public function mostrar(){
 if (session()->has('S_Rol') ) {
 	if (session('S_Rol')==3){
 		$correo_Electronico=session('S_identificador');
+		
 		$consultaidusuario = DB::select('select usuario.idusuario from usuario inner join persona on usuario.fkpersona=persona.idpersona where persona.correoelectronico=?',[$correo_Electronico]);
 		$idusuarioconvert= json_decode( json_encode($consultaidusuario), true);
 		$idusuario = implode($idusuarioconvert[0]);
-	
-	$consultaSeguimiento=DB::select('select foliopedido,pedido.fecha,descuento,paqueteria.nombre, producto.nombreproducto, estadopedido.nombre_Estado from pedido 
-                                    inner join detallepedido on pedido.foliopedido=detallepedido.fkfoliopedido 
-                                    inner join paqueteria on pedido.fkidpaqueteria=paqueteria.idpaqueteria 
-                                    inner join producto on detallepedido.fkproducto=producto.idproducto 
-                                    inner join historialpedido on pedido.foliopedido=historialpedido.fkfoliopedido 
+
+	$consultaSeguimiento=DB::select('select foliopedido,pedido.fecha,descuento,paqueteria.nombre, producto.nombreproducto, estadopedido.nombre_Estado from pedido
+                                    inner join detallepedido on pedido.foliopedido=detallepedido.fkfoliopedido
+                                    inner join paqueteria on pedido.fkidpaqueteria=paqueteria.idpaqueteria
+                                    inner join producto on detallepedido.fkproducto=producto.idproducto
+                                    inner join historialpedido on pedido.foliopedido=historialpedido.fkfoliopedido
                                     inner join estadopedido on historialpedido.fkestadopedido=estadopedido.idestadopedido
-                                    where pedido.fkidusuario=?',[$idusuario]); 
-									
-	$fechaB = DB::select('select pedido.fecha from pedido 
-														inner join detallepedido on pedido.foliopedido=detallepedido.fkfoliopedido 
-                                    inner join paqueteria on pedido.fkidpaqueteria=paqueteria.idpaqueteria 
-                                    inner join producto on detallepedido.fkproducto=producto.idproducto 
-                                    inner join historialpedido on pedido.foliopedido=historialpedido.fkfoliopedido 
+                                    where pedido.fkidusuario=?',[$idusuario]);
+
+	$fechaB = DB::select('select pedido.fecha from pedido
+														inner join detallepedido on pedido.foliopedido=detallepedido.fkfoliopedido
+                                    inner join paqueteria on pedido.fkidpaqueteria=paqueteria.idpaqueteria
+                                    inner join producto on detallepedido.fkproducto=producto.idproducto
+                                    inner join historialpedido on pedido.foliopedido=historialpedido.fkfoliopedido
                                     inner join estadopedido on historialpedido.fkestadopedido=estadopedido.idestadopedido
 														where pedido.fkidusuario=?',[$idusuario]);
-				
+
 				if ($fechaB!=null)
 				{
-																		
+
 				$fechaConversion = json_decode(json_encode($fechaB),true);
 				$fechaconvertida = implode($fechaConversion[0]);
 
 					$fechaBien = date("d/m/Y", strtotime($fechaconvertida));
-	
+
 					return view ('compras_Realizadas',['consultaSeguimiento'=>$consultaSeguimiento, 'fechaBien'=>$fechaBien]);
 				}
 				else
@@ -48,11 +49,11 @@ if (session()->has('S_Rol') ) {
 						$consultaSeguimiento = "0";
 					return view ('compras_Realizadas',['consultaSeguimiento'=>$consultaSeguimiento]);
 				}
-														
-						
+
+
 	}
 
-}	
+}
 return redirect ('/');
 }
 
--- iravi/app/Http/Controllers/seguimientoPedidos.php
+++ iravi/app/Http/Controllers/seguimientoPedidos.php
@@ -9,32 +9,32 @@ use DB;
 class seguimientoPedidos extends Controller
 {
 
-	
+
 public function mostrar(){
 if (session()->has('S_Rol') ) {
     $rutaPaginacion="seguimientoPedidos-pagina";
         //Para sacar los elementos de la primera pagina
         $inicio=0;
         $paginaActual=1;
-        $pedidosPorPagina=9;
-	
+        $pedidosPorPagina=2;
+
 	if (session('S_Rol')==1 ) {
 	    $header="Todos los pedidos";
 	    $estado1= DB::select('SELECT count(h.fecha) as c FROM historialpedido h, pedido p where fkestadopedido=1 and fkestadopedido in(select max(fkestadopedido) from historialpedido where fkfoliopedido=p.foliopedido order by fecha desc) and p.foliopedido=h.fkfoliopedido;');
         $estado2= DB::select('SELECT count(h.fecha) as c FROM historialpedido h, pedido p where fkestadopedido=2 and fkestadopedido in(select max(fkestadopedido) from historialpedido where fkfoliopedido=p.foliopedido order by fecha desc) and p.foliopedido=h.fkfoliopedido;');
         $estado3= DB::select('SELECT count(h.fecha) as c FROM historialpedido h, pedido p where fkestadopedido=3 and fkestadopedido in(select max(fkestadopedido) from historialpedido where fkfoliopedido=p.foliopedido order by fecha desc) and p.foliopedido=h.fkfoliopedido;');
         $estado4= DB::select('SELECT count(h.fecha) as c FROM historialpedido h, pedido p where fkestadopedido=4 and fkestadopedido in(select max(fkestadopedido) from historialpedido where fkfoliopedido=p.foliopedido order by fecha desc) and p.foliopedido=h.fkfoliopedido;');
-       
 
+//ss
  //
-        $pedidos=DB::select('SELECT p.foliopedido,concat(pe.nombrepersona," ",pe.apellidopaterno," ",pe.apellidomaterno) as nombre,concat(d.calle," #",d.numint, ", ", d.ciudad,", ", d.pais,", ",d.codigopostal)as direccion ,p.fecha, (p.subtotal-p.descuento) as total, e.nombre_Estado as estadopedido from pedido p, paqueteria pa, persona pe, usuario u, direccion d, estadopedido e where p.fkidpaqueteria=pa.idpaqueteria and u.fkpersona=pe.idpersona and p.fkidusuario=u.idusuario and p.fkiddireccion=d.iddireccion and e.idestadopedido in(select max(fkestadopedido) from historialpedido where fkfoliopedido=p.foliopedido order by fecha desc) order by fecha desc 
-            limit ?,?',
+        $pedidos=DB::select('SELECT p.foliopedido,concat(pe.nombrepersona," ",pe.apellidopaterno," ",pe.apellidomaterno) as nombre,concat(d.calle," #  " ,d.numint, ", ", d.ciudad,", ", d.pais,", ",d.codigopostal)as direccion ,p.fecha, (p.subtotal-p.descuento) as total, e.nombre_Estado as estadopedido from pedido p, paqueteria pa, persona pe, usuario u, direccion d, estadopedido e where p.fkidpaqueteria=pa.idpaqueteria and u.fkpersona=pe.idpersona and p.fkidusuario=u.idusuario and p.fkiddireccion=d.iddireccion and e.idestadopedido in(select max(fkestadopedido) from historialpedido where fkfoliopedido=p.foliopedido order by fecha desc) order by fecha desc
+            limit ?,?' ,
             [$inicio,$pedidosPorPagina]);
-//
+
 
 
         $numeroPaginas=DB::select('SELECT p.foliopedido,concat(pe.nombrepersona," ",pe.apellidopaterno," ",pe.apellidomaterno) as nombre,concat(d.calle," #",d.numint, ", ", d.ciudad,", ", d.pais,", ",d.codigopostal)as direccion ,p.fecha, (p.subtotal-p.descuento) as total, e.nombre_Estado as estadopedido from pedido p, paqueteria pa, persona pe, usuario u, direccion d, estadopedido e where p.fkidpaqueteria=pa.idpaqueteria and u.fkpersona=pe.idpersona and p.fkidusuario=u.idusuario and p.fkiddireccion=d.iddireccion and e.idestadopedido in(select max(fkestadopedido) from historialpedido where fkfoliopedido=p.foliopedido order by fecha desc) order by fecha desc '
-          
+
            );
 
     $numeroPaginas=count($numeroPaginas);
@@ -45,13 +45,60 @@ if (session()->has('S_Rol') ) {
 		return view ('seguimiento_Pedidos',['pedidos'=>$pedidos,'header'=>$header,'estado1'=>$estado1,'estado2'=>$estado2,'estado3'=>$estado3,'estado4'=>$estado4,
          'numeroPaginas'=>$numeroPaginas,'paginaActual'=>$paginaActual, 'rutaPaginacion'=>$rutaPaginacion]);
 	}
-   
+
 }
 
 return redirect ('/');
 }
 
-public function filtrar(Request $data){
+
+public function mostrarPaginacion($paginaActual) {
+    if (session()->has('S_Rol') ) {
+    $rutaPaginacion="seguimientoPedidos-pagina";
+        //Para sacar los elementos de la primera pagina
+    $pedidosPorPagina=2;
+        $inicio=$paginaActual*$pedidosPorPagina-$pedidosPorPagina;
+
+
+    if (session('S_Rol')==1 ) {
+        $header="Todos los pedidos";
+        $estado1= DB::select('SELECT count(h.fecha) as c FROM historialpedido h, pedido p where fkestadopedido=1 and fkestadopedido in(select max(fkestadopedido) from historialpedido where fkfoliopedido=p.foliopedido order by fecha desc) and p.foliopedido=h.fkfoliopedido;');
+        $estado2= DB::select('SELECT count(h.fecha) as c FROM historialpedido h, pedido p where fkestadopedido=2 and fkestadopedido in(select max(fkestadopedido) from historialpedido where fkfoliopedido=p.foliopedido order by fecha desc) and p.foliopedido=h.fkfoliopedido;');
+        $estado3= DB::select('SELECT count(h.fecha) as c FROM historialpedido h, pedido p where fkestadopedido=3 and fkestadopedido in(select max(fkestadopedido) from historialpedido where fkfoliopedido=p.foliopedido order by fecha desc) and p.foliopedido=h.fkfoliopedido;');
+        $estado4= DB::select('SELECT count(h.fecha) as c FROM historialpedido h, pedido p where fkestadopedido=4 and fkestadopedido in(select max(fkestadopedido) from historialpedido where fkfoliopedido=p.foliopedido order by fecha desc) and p.foliopedido=h.fkfoliopedido;');
+
+
+ //
+        $pedidos=DB::select('SELECT p.foliopedido,concat(pe.nombrepersona," ",pe.apellidopaterno," ",pe.apellidomaterno) as nombre,concat(d.calle," #",d.numint, ",", d.ciudad,",", d.pais,",",d.codigopostal)as direccion ,p.fecha, (p.subtotal-p.descuento) as total, e.nombre_Estado as estadopedido from pedido p, paqueteria pa, persona pe, usuario u, direccion d, estadopedido e where p.fkidpaqueteria=pa.idpaqueteria and u.fkpersona=pe.idpersona and p.fkidusuario=u.idusuario and p.fkiddireccion=d.iddireccion and e.idestadopedido in(select max(fkestadopedido) from historialpedido where fkfoliopedido=p.foliopedido order by fecha desc) order by fecha desc
+            limit ?,?'
+						,[$inicio,$pedidosPorPagina]);
+//
+
+
+        $numeroPaginas=DB::select('SELECT p.foliopedido,concat(pe.nombrepersona," ",pe.apellidopaterno," ",pe.apellidomaterno) as nombre,concat(d.calle," #",d.numint, ", ", d.ciudad,", ", d.pais,", ",d.codigopostal)as direccion ,p.fecha, (p.subtotal-p.descuento) as total, e.nombre_Estado as estadopedido from pedido p, paqueteria pa, persona pe, usuario u, direccion d, estadopedido e where p.fkidpaqueteria=pa.idpaqueteria and u.fkpersona=pe.idpersona and p.fkidusuario=u.idusuario and p.fkiddireccion=d.iddireccion and e.idestadopedido in(select max(fkestadopedido) from historialpedido where fkfoliopedido=p.foliopedido order by fecha desc) order by fecha desc '
+
+           );
+
+    $numeroPaginas=count($numeroPaginas);
+     $numeroPaginas=ceil($numeroPaginas/$pedidosPorPagina);
+
+
+
+        return view ('seguimiento_Pedidos',['pedidos'=>$pedidos,'header'=>$header,'estado1'=>$estado1,'estado2'=>$estado2,'estado3'=>$estado3,'estado4'=>$estado4,
+         'numeroPaginas'=>$numeroPaginas,'paginaActual'=>$paginaActual, 'rutaPaginacion'=>$rutaPaginacion]);
+    }
+
+}
+
+return redirect ('/');
+
+
+    }
+
+
+
+
+public function filtrar(Request $data) {
     $estado=$data->input('estado');
         if (session()->has('S_Rol') ) {
 
@@ -73,55 +120,11 @@ public function filtrar(Request $data){
 
                 return view ('seguimiento_Pedidos',['pedidos'=>$pedidos,'header'=>$header,'estado1'=>$estado1,'estado2'=>$estado2,'estado3'=>$estado3,'estado4'=>$estado4]  );
             }
-
-        }
-
-        return redirect ('/');
-    }
+						return redirect ('/');
+  }
 
 
 
-    public function mostrarPaginacion($paginaActual){
-    if (session()->has('S_Rol') ) {
-    $rutaPaginacion="seguimientoPedidos-pagina";
-        //Para sacar los elementos de la primera pagina
-    $pedidosPorPagina=9;
-        $inicio=$paginaActual*$pedidosPorPagina-$pedidosPorPagina;
-        
-    
-    if (session('S_Rol')==1 ) {
-        $header="Todos los pedidos";
-        $estado1= DB::select('SELECT count(h.fecha) as c FROM historialpedido h, pedido p where fkestadopedido=1 and fkestadopedido in(select max(fkestadopedido) from historialpedido where fkfoliopedido=p.foliopedido order by fecha desc) and p.foliopedido=h.fkfoliopedido;');
-        $estado2= DB::select('SELECT count(h.fecha) as c FROM historialpedido h, pedido p where fkestadopedido=2 and fkestadopedido in(select max(fkestadopedido) from historialpedido where fkfoliopedido=p.foliopedido order by fecha desc) and p.foliopedido=h.fkfoliopedido;');
-        $estado3= DB::select('SELECT count(h.fecha) as c FROM historialpedido h, pedido p where fkestadopedido=3 and fkestadopedido in(select max(fkestadopedido) from historialpedido where fkfoliopedido=p.foliopedido order by fecha desc) and p.foliopedido=h.fkfoliopedido;');
-        $estado4= DB::select('SELECT count(h.fecha) as c FROM historialpedido h, pedido p where fkestadopedido=4 and fkestadopedido in(select max(fkestadopedido) from historialpedido where fkfoliopedido=p.foliopedido order by fecha desc) and p.foliopedido=h.fkfoliopedido;');
-       
 
- //
-        $pedidos=DB::select('SELECT p.foliopedido,concat(pe.nombrepersona," ",pe.apellidopaterno," ",pe.apellidomaterno) as nombre,concat(d.calle," #",d.numint, ", ", d.ciudad,", ", d.pais,", ",d.codigopostal)as direccion ,p.fecha, (p.subtotal-p.descuento) as total, e.nombre_Estado as estadopedido from pedido p, paqueteria pa, persona pe, usuario u, direccion d, estadopedido e where p.fkidpaqueteria=pa.idpaqueteria and u.fkpersona=pe.idpersona and p.fkidusuario=u.idusuario and p.fkiddireccion=d.iddireccion and e.idestadopedido in(select max(fkestadopedido) from historialpedido where fkfoliopedido=p.foliopedido order by fecha desc) order by fecha desc 
-            limit ?,?',
-            [$inicio,$pedidosPorPagina]);
-//
-
-
-        $numeroPaginas=DB::select('SELECT p.foliopedido,concat(pe.nombrepersona," ",pe.apellidopaterno," ",pe.apellidomaterno) as nombre,concat(d.calle," #",d.numint, ", ", d.ciudad,", ", d.pais,", ",d.codigopostal)as direccion ,p.fecha, (p.subtotal-p.descuento) as total, e.nombre_Estado as estadopedido from pedido p, paqueteria pa, persona pe, usuario u, direccion d, estadopedido e where p.fkidpaqueteria=pa.idpaqueteria and u.fkpersona=pe.idpersona and p.fkidusuario=u.idusuario and p.fkiddireccion=d.iddireccion and e.idestadopedido in(select max(fkestadopedido) from historialpedido where fkfoliopedido=p.foliopedido order by fecha desc) order by fecha desc '
-          
-           );
-
-    $numeroPaginas=count($numeroPaginas);
-     $numeroPaginas=ceil($numeroPaginas/$pedidosPorPagina);
-
-
-
-        return view ('seguimiento_Pedidos',['pedidos'=>$pedidos,'header'=>$header,'estado1'=>$estado1,'estado2'=>$estado2,'estado3'=>$estado3,'estado4'=>$estado4,
-         'numeroPaginas'=>$numeroPaginas,'paginaActual'=>$paginaActual, 'rutaPaginacion'=>$rutaPaginacion]);
-    }
-   
 }
-
-return redirect ('/');
-
-
-    }
-
 }

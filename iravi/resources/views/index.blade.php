0b1c9a60783f2001c531f2bf214b2d88115edead
José Cordero Alvarado <48689264+joseCorderoAlvarado@users.noreply.github.com>
2019-11-26 20:13:05 -0600

    index

--- iravi/resources/views/index.blade.php
+++ iravi/resources/views/index.blade.php
@@ -35,25 +35,24 @@
 @foreach ($tablaProductos as $producto)
 <div class="col-md-4">
 <div class="card2" style="width: 15rem;">
-  <a> 
+  <a>
   <img class="card-img-top" src="storage{{$producto->ruta}}" alt="Card image cap" style="height:230px;">
   </a>
   <div class="card-body">
     <h5 class="card-title">{{$producto->nombreproducto}}</h5>
     <p class="card-text">${{$producto->precio}}</p>
+	@endforeach
     <a class="btn btn-primary" data-toggle="modal" data-target="#mensaje">Añadir al carrito</a>
-    
     <div id="mensaje" class="modal fade">
     	<div class="modal-dialog">
         	<div class="modal-content">
-           	
             	<h4 class="modal-title">A&uacute;n no inicias sesi&oacute;n</h4>
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
             </div>
            <div class="modal-footer">
            	<form  action="Alerta" method="post" enctype="multipart/form-data">
 			{{ csrf_field() }}
-	        
+
 					<button id="button" class="btn btn-primary" style="background-color: #003669; color: white;">Iniciar sesi&oacute;n</button>
 	        </form>
 	        <form  action="Alerta1" method="post" enctype="multipart/form-data">
@@ -67,7 +66,6 @@
   </div>
 </div>
 </div>
-@endforeach
 	{{------------------------Fin de Cosas que se repiten---------------------------}}
 
 </div>

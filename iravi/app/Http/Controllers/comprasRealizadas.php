f3aa3ec11bbad7e65c31388e40e653619901c1d8
Javier-Ramirez <javi.guti.rama@gmail.com>
2019-11-26 22:57:28 -0600

    COmpras realizadas controlador

--- iravi/app/Http/Controllers/comprasRealizadas.php
+++ iravi/app/Http/Controllers/comprasRealizadas.php
@@ -9,36 +9,36 @@ use DB;
 class comprasRealizadas extends Controller
 {
 
-public function mostrar(){
-if (session()->has('S_Rol') ) {
-	if (session('S_Rol')==3){
-		$correo_Electronico=session('S_identificador');
-		
-		$consultaidusuario = DB::select('select usuario.idusuario from usuario inner join persona on usuario.fkpersona=persona.idpersona where persona.correoelectronico=?',[$correo_Electronico]);
-		$idusuarioconvert= json_decode( json_encode($consultaidusuario), true);
-		$idusuario = implode($idusuarioconvert[0]);
+	public function mostrar()
+	{
+		if (session()->has('S_Rol') ) 
+		{
+			if (session('S_Rol')==3)
+			{
+				$correo_Electronico=session('S_identificador');
+				
+				$consultaidusuario = DB::select('select usuario.idusuario from usuario inner join persona on usuario.fkpersona=persona.idpersona where persona.correoelectronico=?',[$correo_Electronico]);
+				$idusuarioconvert= json_decode( json_encode($consultaidusuario), true);
+				$idusuario = implode($idusuarioconvert[0]);
 
-	$consultaSeguimiento=DB::select('select foliopedido,pedido.fecha,descuento,paqueteria.nombre, producto.nombreproducto, estadopedido.nombre_Estado from pedido
-                                    inner join detallepedido on pedido.foliopedido=detallepedido.fkfoliopedido
-                                    inner join paqueteria on pedido.fkidpaqueteria=paqueteria.idpaqueteria
-                                    inner join producto on detallepedido.fkproducto=producto.idproducto
-                                    inner join historialpedido on pedido.foliopedido=historialpedido.fkfoliopedido
-                                    inner join estadopedido on historialpedido.fkestadopedido=estadopedido.idestadopedido
-                                    where pedido.fkidusuario=?',[$idusuario]);
+				$consultaSeguimiento=DB::select('SELECT distinct pedido.foliopedido,pedido.fecha,descuento,paqueteria.nombre, estadopedido.nombre_Estado   
+													from pedido 
+													inner join paqueteria on pedido.fkidpaqueteria=paqueteria.idpaqueteria
+													inner join historialpedido on pedido.foliopedido=historialpedido.fkfoliopedido
+													inner join estadopedido on historialpedido.fkestadopedido=estadopedido.idestadopedido
+												    where fkidusuario =?',[$idusuario]);
 
-	$fechaB = DB::select('select pedido.fecha from pedido
-														inner join detallepedido on pedido.foliopedido=detallepedido.fkfoliopedido
-                                    inner join paqueteria on pedido.fkidpaqueteria=paqueteria.idpaqueteria
-                                    inner join producto on detallepedido.fkproducto=producto.idproducto
-                                    inner join historialpedido on pedido.foliopedido=historialpedido.fkfoliopedido
-                                    inner join estadopedido on historialpedido.fkestadopedido=estadopedido.idestadopedido
-														where pedido.fkidusuario=?',[$idusuario]);
+				$fechaB = DB::select('SELECT distinct pedido.fecha 
+										from pedido 
+										inner join paqueteria on pedido.fkidpaqueteria=paqueteria.idpaqueteria
+										inner join historialpedido on pedido.foliopedido=historialpedido.fkfoliopedido
+										inner join estadopedido on historialpedido.fkestadopedido=estadopedido.idestadopedido
+									    where fkidusuario =?',[$idusuario]);
 
 				if ($fechaB!=null)
 				{
-
-				$fechaConversion = json_decode(json_encode($fechaB),true);
-				$fechaconvertida = implode($fechaConversion[0]);
+					$fechaConversion = json_decode(json_encode($fechaB),true);
+					$fechaconvertida = implode($fechaConversion[0]);
 
 					$fechaBien = date("d/m/Y", strtotime($fechaconvertida));
 
@@ -46,17 +46,12 @@ if (session()->has('S_Rol') ) {
 				}
 				else
 				{
-						$consultaSeguimiento = "0";
+					$consultaSeguimiento = "0";
 					return view ('compras_Realizadas',['consultaSeguimiento'=>$consultaSeguimiento]);
 				}
-
-
+			}
+		}
+		return redirect ('/');
 	}
-
-}
-return redirect ('/');
-}
-
-
 }
 ?>

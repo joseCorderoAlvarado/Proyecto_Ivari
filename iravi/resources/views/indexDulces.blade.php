11d35bc958b3269cc5b31b3d1875d0028b8a70bb
José Cordero Alvarado <48689264+joseCorderoAlvarado@users.noreply.github.com>
2019-12-01 09:23:41 -0600

    vista productos

--- iravi/resources/views/indexDulces.blade.php
+++ iravi/resources/views/indexDulces.blade.php
@@ -39,7 +39,7 @@
 	  <div class="card-body">
 	    <h5 class="card-title">{{$producto->nombreproducto}}</h5>
 	    <p class="card-text">${{$producto->precio}}</p>
-	    <a href="#" class="btn btn-primary">Añadir al carrito</a>
+	   <a class="btn btn-primary"  href="vistaProducto{{$producto->idproducto}}" style="background-color: #003669; color: white;">Ver producto</a>
 	  </div>
 	</div>
 	</div>
@@ -87,8 +87,8 @@
 
 				@if ($i==$numeroPaginas)
 						@if ($paginaActual==$numeroPaginas)
-			        <li 
-				
+			        <li
+
 				class="page-item disabled">
 								<a class="page-link" href="#" aria-label="Next">
 								<span aria-hidden="true">&raquo;</span>

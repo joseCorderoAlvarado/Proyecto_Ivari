28d8e62106e6a13855b14253437d79965bf84781
José Cordero Alvarado <48689264+joseCorderoAlvarado@users.noreply.github.com>
2019-12-09 20:37:13 -0600

    menus

--- iravi/resources/views/layouts/menu_Navegacion.blade.php
+++ iravi/resources/views/layouts/menu_Navegacion.blade.php
@@ -3,8 +3,8 @@
 <div class="row"> 
 <div class="col-md-12">
 <nav class="navbar navbar-expand-lg	navbar-light bg-light" style="background-color: #003366 !important">
-    <img src="images/iravi.png" width="30" height="30"	class="d-inline-block align-top" alt="">
-	<a	class="navbar-brand" href="#" style="color: #fff"><b>Artesanias Iravi</b></a>
+    <img  href="{{ url('/')}}" src="images/iravi.png" width="30" height="30"	class="d-inline-block align-top" alt="">
+	<a	class="navbar-brand" href="{{ url('/')}}" style="color: #fff"><b>Artesanias Iravi</b></a>
 	<button	class="navbar-toggler"	type="button"	data-toggle="collapse"data-target="#navbarSupportedContent"	aria-controls="navbarSupportedContent" aria-expanded="false"	aria-label="Toggle	navigation">
 	<span class="navbar-toggler-icon"></span>
 	</button>
